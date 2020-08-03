9d6f4ec6785c01d8fecc7aa80c5e94d0cfa94fe4
Maciej Tyski <maciej.tyski@pl.abb.com>
2020-08-03 15:44:42 +0200

    Added pipeline behavior, TODO: result monad

---
+++ TodoApi/TodoApi.Services/PipelineBehavior/RequestValidationPipelineBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using FluentValidation;
+
+using MediatR;
+
+namespace TodoApi.Services.PipelineBehavior
+{
+    internal class RequestValidationPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IList<IValidator<TRequest>> validators;
+
+        public RequestValidationPipelineBehavior(IList<IValidator<TRequest>> validators)
+        {
+            this.validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var failures = this.validators.Select(v => v.Validate(context))
+                .SelectMany(vr => vr.Errors)
+                .Select(ve => ve.ErrorMessage)
+                .ToList();
+
+            return failures.Any()
+                       ? Result.Failure<Task<TResponse>>(string.Join($", {Environment.NewLine}", failures))
+                       : next();
+        }
+    }
+}
